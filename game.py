a3a77b0ff6918658270d9af64dc76355caa5015e
Raunak Jain <raunakjain1002@gmail.com>
2025-04-30 00:54:10 +0530

    Improvements Made (Issue 1).py
    Improvements Made:
    
    1. Uses numbers instead of full words.
    2. Clean output formatting with spacing and capitalized choices.
    3. Organized with functions.
    4. Easier to expand or loop later.

--- game.py
+++ game.py
@@ -1,47 +1,61 @@
-# All LIbraries Installed :- 
-import os 
+# All Libraries Installed :- 
 import random
+import os
 
-# For Continuation :- 
+# Displaying The Virtual Interface :- 
+print("Welcome To The 'Rock , Paper and Scissors Game'")
+print("1. Play The Game")
+print("2. Exit The Game")
+choice = int(input("What would you like to do ? (1/2) :- "))
+
+
 while True:
- choices = ["rock" , "paper" , "scissor"]
- comp_decision = random.choice(choices)
+# If User Don't Want To Play The Game :-
+    if choice == 2:
+      
+      print('Thank You for Playing .')
+      print("Have a Good Day . ")
+      os._exit(1)
 
-# Display The Virtual Interface :- 
- print("Welcome To The 'Rock , Paper and Scissors Game' " )
- print("1. Play The Game")
- print("2. Exit The Game")
- choice = int(input("What would you like to do ? (1/2) :- "))
+# If User Wants To Play The Game :-
+    elif choice == 1:
+      print("\nChoose:")
+      print("1. Rock")
+      print("2. Paper")
+      print("3. Scissors")
 
-# If User Wants To Play The Game :- 
+      user_input = input("Enter your choice (1/2/3/): ")
 
- if choice == 1:
-    print("What You Want To Choose ( Rock / Paper / Scissors ) :- ")
-    choose = input("Enter Your Decision :- ").lower()
-    if choose not in choices :
-        print("Input Is Invalid .. Please Try Again ..")
-    elif choose in choices :
-        if comp_decision == choose :
-            print("Since you both have choosen ",comp_decision,"... Its a Tie !!!")
-        elif choose == "rock" and comp_decision == "scissor":
-            print("Since Computer Chooses",comp_decision,"Congratulations .. You win!")
-        elif choose == "paper" and comp_decision == "Rock":
-            print("Since Computer Chooses",comp_decision,"Congratulations .. You Win! ")
-        elif choose == "scissor" and comp_decision == "paper":
-            print("Since Computer Chooses",comp_decision,"Congratulations .. You Win! ")
-        else:
-            print("Sorry . You Lost The Game . Try Again Later ")
+      while user_input not in ['1', '2', '3']:
+        user_input = input("Invalid choice. Enter 1, 2 or 3: ")
+
+      user_choice = int(user_input)
+      comp_choice = random.randint(1, 3)
+
+      choices = {1: "Rock", 2: "Paper", 3: "Scissors"}
+
+      user_word = choices[user_choice]
+      comp_word = choices[comp_choice]
+
+      print("You chose:", user_word)
+      print("Computer chose:", comp_word)
+
+      if user_choice == comp_choice:
+        print("It's a tie!")
+      elif (user_choice == 1 and comp_choice == 3) or (user_choice == 2 and comp_choice == 1) or (user_choice == 3 and comp_choice == 2):
+        print("You win!")
+      else:
+        print("You lose!")
+
+# For An Invalid Input :-
     else:
-       print("Sorry , It is not defined in our program . ")
-    
-# If User Wants To Exit The Game :- 
- elif choice == 2:
-    print('Thank You for Playing .')
-    print("Have a Good Day . ")
-    os._exit(1)
+      print("Sorry we are not able to help you . ")
+      print("Please write your query on raunakjain1002@gmail.com")
+      print("Thank You .")
+      os.exit(1)
 
-# For An Invalid Input :- 
- else:
-    print("Sorry we are not able to help you . ")
-    print("Please write your query on raunakjain1002@gmail.com")
-    print("Thank You .")
+# If User wants to Play Again :- 
+    print("\nPlay again or exit:")
+    print("1. Play Again")
+    print("2. Exit")
+    choice = int(input("Your choice (1/2): "))
